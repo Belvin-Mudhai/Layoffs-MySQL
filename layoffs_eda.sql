4270cc4115ed8807ecc9e1edffa6d4975b79e629
Belvin Mudhai <belvin.bm@gmail.com>
2024-04-26 01:10:27 +0300

    Deeper analysis of the data, delving into the rate of layoffs per year and the companies and industries that made most of these layoffs each year.

--- layoffs_eda.sql
+++ layoffs_eda.sql
@@ -38,9 +38,63 @@ GROUP BY stage
 ORDER BY 2 DESC;
 --  Companies in the Post IPO stage were the most affected by the downsizing and closures
 
+SELECT MIN(`date`)
+FROM layoffs_staging2;
+-- The data dates back from the beginning of the pandemic
 
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 2 DESC;
+-- 2022 had the most layoffs recorded yet
 
+SELECT SUBSTRING(`date`, 1, 7) AS `Month`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`, 1, 7) IS NOT NULL
+GROUP BY `Month`
+ORDER BY 1 ASC;
+-- The trend of the job layoffs
 
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+-- The order that companies did lay offs through the years
+
+WITH Company_Year (Company, Years, Total) AS 
+(SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`))
+SELECT *, DENSE_RANK() OVER(PARTITION BY Years ORDER BY Total DESC) AS Yearly_Ranking
+FROM Company_Year
+WHERE Years AND Total IS NOT NULL;
+-- Assessed the order of companies that made most layoffs per year
+
+-- A look at the companies with most layoffs through the years
+WITH Company_Year (Company, Years, Total) AS 
+(SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)),
+Company_Year_Rank AS (
+SELECT *, DENSE_RANK() OVER(PARTITION BY Years ORDER BY Total DESC) AS Yearly_Ranking
+FROM Company_Year
+WHERE Years AND Total IS NOT NULL)
+SELECT *
+FROM Company_Year_Rank
+WHERE Yearly_Ranking <= 5;
+
+-- A look at the industries with most layoffs through the years
+WITH Industry_Year (industry, Years, Total) AS 
+(SELECT industry, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry, YEAR(`date`)),
+Industry_Year_Rank AS (
+SELECT *, DENSE_RANK() OVER(PARTITION BY Years ORDER BY Total DESC) AS Yearly_Ranking
+FROM Industry_Year
+WHERE Years AND Total IS NOT NULL)
+SELECT *
+FROM Industry_Year_Rank
+WHERE Yearly_Ranking <= 5;
 
 
 

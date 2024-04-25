2b862a367010130d1825d8eb6160fb197e0fe33f
Belvin Mudhai <belvin.bm@gmail.com>
2024-04-25 23:50:23 +0300

    Assessment of the stages at which the companies were laying employees off

--- layoffs_eda.sql
+++ layoffs_eda.sql
@@ -1,3 +1,38 @@
 USE world_layoffs;
+
+--  EDA
 SELECT *
 FROM layoffs_staging2;
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+	AND total_laid_off IS NOT NULL;
+
+-- Most affected companies and the workforce involved
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off > (SELECT AVG(total_laid_off)
+						FROM layoffs_staging2)
+				AND percentage_laid_off = 1
+ORDER BY total_laid_off DESC;
+                
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country, industry
+ORDER BY 2 DESC;
+-- USA has the most number of layoffs
+
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+-- Consumer and Retail are industries with most layoffs
+
+
+
+
+
